3044d16790674742883585239b731726772e74be
Johnny Sung <j796160836@yahoo.com.tw>
2012-03-13 19:39:36 +0800

    上傳HttpRequestTest1 (NSURLConnection版本)

--- HttpRequestTest1/ViewController.h
+++ HttpRequestTest1/ViewController.h
@@ -8,6 +8,28 @@
 
 #import <UIKit/UIKit.h>
 
+// http://www.01-labor.com/2011/07/nsurlconnectionhttp.html
+// http://stm237.iteye.com/blog/1005752
+//在iPhone上建立HTTP連線(GET/POST)
+
 @interface ViewController : UIViewController
+{
+    NSMutableData *responseData;
+}
+
+// 開始接收資料，會呼叫此方法
+- (void)connection:(NSURLConnection *)connection didReceiveResponse:(NSURLResponse *)response;
+// 接收新的資料時，會呼叫此方法
+- (void)connection:(NSURLConnection *)connection didReceiveData:(NSData *)data;
+// 下載完畢時，會呼叫此方法
+- (void)connectionDidFinishLoading:(NSURLConnection *)connection;
+// 連線錯誤時，會呼叫此方法
+- (void)connection:(NSURLConnection *)connection didFailWithError:(NSError *)error;
+
+- (IBAction)request_btn_onclick:(id)sender;
+- (IBAction)request2_btn_onclick:(id)sender;
+
+@property (retain, nonatomic) IBOutlet UILabel *label;
+
 
 @end
--- HttpRequestTest1/ViewController.m
+++ HttpRequestTest1/ViewController.m
@@ -9,6 +9,9 @@
 #import "ViewController.h"
 
 @implementation ViewController
+@synthesize label;
+
+
 
 - (void)didReceiveMemoryWarning
 {
@@ -16,16 +19,91 @@
     // Release any cached data, images, etc that aren't in use.
 }
 
-#pragma mark - View lifecycle
 
+
+#pragma mark - Connection delegate
+
+- (void)connection:(NSURLConnection *)connection didReceiveResponse:(NSURLResponse *)response
+{
+    // 開始下載，重置responseData資料
+    NSLog(@"didReceiveResponse");
+    [responseData setLength:0];
+}
+- (void)connection:(NSURLConnection *)connection didReceiveData:(NSData *)data
+{
+    // 下載中，附加資料
+    [responseData appendData:data];
+}
+- (void)connectionDidFinishLoading:(NSURLConnection *)connection
+{
+    // 下載完成，釋放responseData
+    [connection release];
+    NSLog (@"succeeded  %d byte received", [responseData length]); 
+    // 轉譯編碼文字
+    NSString *responseString = [[NSString alloc] initWithData:responseData encoding:NSUTF8StringEncoding];
+    
+    [responseData release];
+    
+    label.text =  responseString;
+    [responseString release];
+}
+- (void)connection:(NSURLConnection *)connection didFailWithError:(NSError *)error
+{
+    // 下載錯誤
+    label.text = [NSString stringWithFormat:@"Connection failed: %@",[error description]];
+    NSLog(@"Connection failed! Error - %@ %@", 
+          [error localizedDescription], 
+          [[error userInfo] objectForKey:NSURLErrorFailingURLStringErrorKey]);
+}
+
+- (IBAction)request_btn_onclick:(id)sender {
+    
+    responseData = [[NSMutableData data] retain];
+    NSString *url = [NSString stringWithFormat:@"http://127.0.0.1/httptest/t.php"];
+    NSURLRequest *request = [NSURLRequest requestWithURL:[NSURL URLWithString:url ]];
+    [[NSURLConnection alloc] initWithRequest:request delegate:self];
+    
+}
+
+- (IBAction)request2_btn_onclick:(id)sender {
+    responseData = [[NSMutableData data] retain];
+    NSString *url = [NSString stringWithFormat:@"http://127.0.0.1/"];
+    NSURLRequest *request = [NSURLRequest requestWithURL:[NSURL URLWithString:url ]];
+    [[NSURLConnection alloc] initWithRequest:request delegate:self];
+    
+    
+}
+
+
+
+
+#pragma mark - View lifecycle
 - (void)viewDidLoad
 {
     [super viewDidLoad];
+    //    
+    //    NSString *str = @"http://my_server/get.php?name=brian&gender=male";
+    //    //網址編碼
+    //    NSString *str_enc = [str stringByAddingPercentEscapesUsingEncoding:NSUTF8StringEncoding];
+    //    // 使用NSURL物件
+    //    NSURL *url = [[NSURL URLWithString:str_enc] retain];
+    //    
+    //    NSURLRequest *req = [NSURLRequest requestWithURL:url];
+    //    NSURLConnection *conn = [[NSURLConnection alloc] initWithRequest:req delegate:self];
+    //    
 	// Do any additional setup after loading the view, typically from a nib.
 }
 
+
+
+
+
+
+
+
 - (void)viewDidUnload
 {
+    [self setLabel:nil];
     [super viewDidUnload];
     // Release any retained subviews of the main view.
     // e.g. self.myOutlet = nil;
@@ -57,4 +135,8 @@
     return (interfaceOrientation != UIInterfaceOrientationPortraitUpsideDown);
 }
 
+- (void)dealloc {
+    [label release];
+    [super dealloc];
+}
 @end
